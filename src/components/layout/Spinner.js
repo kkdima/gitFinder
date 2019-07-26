f5689ddd4c4cdc117f8d2304eaa23752c145b743
Dima Kononenko <dimakononenko@Dimas-MacBook-Pro.local>
2019-07-25 20:27:06 -0700

    search and clear button implemented

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";
+import spinner from "./spinner.gif";
 
 const Spinner = () => (
 	<Fragment>
 		<img
-			src="{spinner}"
+			src={ spinner }
 			alt="Loading..."
 			style={{ width: "200px", margin: "auto", display: "block" }}
 		/>
